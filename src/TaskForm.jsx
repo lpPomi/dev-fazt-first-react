abfe2938f707092a60548b5132fc583afd6c7ed7
lppomi <lp.chamba@gmail.com>
2022-10-09 14:58:10 +0200

    move code from TaskForm into App

--- src/TaskForm.jsx
+++ src/TaskForm.jsx
@@ -1,22 +1,41 @@
 
+import { useState } from 'react'
+
+
 function TaskForm() {
+
+
+
+    // save the contend from input in a react state and variable with useState
+    // for the title .. init is empty
+    const [title, setTitle] = useState('')
+
 
     // to avoid refresh page by clock save form
     const handleSubmit = event => {
         // prevent page refresh
         event.preventDefault();
+        console.log(title)
     }
 
-    // to see on cobnsole what we are typing in the input field
+    // to see on console what we are typing in the input field
     const handleChange = (e) => {
         console.log(e.target.value);
+    };
+
+    // to save the title input  into the title variable using setTitle
+    const getTitle = (e) => {
+        setTitle(e.target.value)
+        //console.log(e.target.value);
     };
 
 
 
     return (
         <form onSubmit={handleSubmit}>
-            <input placeholder="create a task" onChange={handleChange} />
+            {/*  <input placeholder="create a task" onChange={handleChange} /> */}
+            <input placeholder="create a task" onChange={getTitle} />
+
             <button onClick={() => {
                 console.log('Hi Button');
             }}>
